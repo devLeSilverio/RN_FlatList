8ce7543657c862b6060e218aaedec2d3b3e0d62c
devLeSilverio <leticia.silverio09@gmail.com>
2021-09-02 05:11:11 -0300

    finishing the style

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Courses from './Components/Courses';
-
 export default class App extends Component{
  
   constructor(props){
     super(props);
     this.state = {
       data:[
-        {id:'1',  name:'JavaScript Avançado', description:'curso técnico de programação', img:''},
-        {id:'2',  name:'Inglês', description:'curso de idiomas', img:''},
-        {id:'3',  name:'Java Intermediário', description:'curso técnico de programação', img:''},
-        {id:'4',  name:'Alemão', description:'curso de idiomas', img:''},
-        {id:'5',  name:'Python Básico', description:'curso técnico de programação', img:''},
+        {id:'1',  name:'JavaScript Avançado', description:'curso técnico de programação', img:'https://usefulangle.com/img/thumb/javascript.png'},
+        {id:'2',  name:'Inglês', description:'curso de idiomas', img:'https://www.simec.com.br/media/img_noticias/Dicas-para-melhorar-o-ingles-1-750x500.jpeg'},
+        {id:'3',  name:'Java Intermediário', description:'curso técnico de programação', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRySvob146hPZnE-pQ4wAhas_8bG_NCG7hQxn3I6vr4nU4g492QVG2CVnyZp7FuxN3aId0&usqp=CAU'},
+        {id:'4',  name:'Alemão', description:'curso de idiomas', img:'http://3.bp.blogspot.com/-MaBMM11qYHM/V2UCIgFRGHI/AAAAAAAAk88/szaPWSqqllceGYPi8QQ4sq22d8ghMEsFgCK4B/s1600/ich_lerne_deutsch.png'},
+        {id:'5',  name:'Python Básico', description:'curso técnico de programação', img:'https://img.olhardigital.com.br/wp-content/uploads/2020/04/20200423030657-1131x450.jpg'},
       ]
     }
   }
@@ -20,10 +19,11 @@ export default class App extends Component{
   render(){
     return (
       <View style={styles.container}>
+        <Text style={styles.text1}>Cursos</Text>
         <FlatList
           data={this.state.data}
           keyExtractor={(item)=>item.id}
-          renderItem={({item})=><NameCourse data={item} />}
+          renderItem={({item})=><Courses data={item} />}
         />
       </View>
     );
@@ -32,9 +32,12 @@ export default class App extends Component{
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     marginTop:40,
   },
+  text1: {
+    fontSize:40,
+    textAlign: 'center',
+    backgroundColor:'#DEC5C5',
+    marginBottom:20,
+  }
 });
--- Components/Courses.js
+++ Components/Courses.js
@@ -1,10 +1,28 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
-export default function(props){
+export default function Courses(props){
     return(
-      <View>
-        <Text>Nome: {this.props.data.name}</Text>
+      <View style={styles.view}>
+        <Text style={styles.title}>{props.data.name}</Text>
+        <Image style={styles.img} source={{uri: props.data.img}}/>
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+ img:{
+   width:200,
+   height:80,
+   borderWidth:1,
+   borderColor:'#000000'
+ },
+ view:{
+    alignItems:'center',
+    marginBottom:20,
+ },
+ title:{
+   fontSize:20,
+   fontWeight:'bold',
+ }
+});
